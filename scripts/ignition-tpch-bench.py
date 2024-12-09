f296ab8118fd99474659166b07511e2a99488d7c
Mateusz Gienieczko <giem@in.tum.de>
2024-12-09 11:23:45 +0100

    added scripts

---
+++ scripts/ignition-tpch-bench.py
@@ -0,0 +1,63 @@
+import os
+import json
+import sys
+
+TMP_DIR = "/tmp/duckdb-ignition-profiling"
+QUERIES = [1, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 17, 18, 19, 20, 21]
+SAMPLES = 10
+
+
+def get_profile_json_path(q):
+    return f"{TMP_DIR}/tpch-q{q}.json"
+
+
+def get_profile_json(q):
+    with open(get_profile_json_path(q), 'r') as file:
+        return json.load(file)
+
+
+def find_ignition_entry(profile):
+    if "extra_info" in profile and "Function" in profile["extra_info"] and profile["extra_info"]["Function"] == "IGNITION":
+        return profile
+    if "children" in profile:
+        for child in profile["children"]:
+            rec_res = find_ignition_entry(child)
+            if rec_res:
+                return rec_res
+    return None
+
+
+if len(sys.argv) != 6:
+    print(f"Usage: {sys.argv[0]} DUCKDB_EXE TPCH_DB SCRIPTS_DIR THREADS OUT_DIR")
+    exit(1)
+
+duckdb_exe = sys.argv[1]
+tpch_db = sys.argv[2]
+scripts_dir = sys.argv[3]
+threads = int(sys.argv[4])
+out_dir = sys.argv[5]
+
+os.system(f"mkdir -p {TMP_DIR}")
+
+samples = {}
+
+for q in QUERIES:
+    input_file = f"{scripts_dir}/tpch-q{q}.sql"
+    sql_file_path = f"{TMP_DIR}/input_q{q}.sql"
+    os.system(f"cat {input_file} {input_file} {input_file} {input_file} {input_file} > {sql_file_path}")
+    os.system(f"echo \"SET threads = {threads};\n\" >> {sql_file_path}")
+    os.system(f"echo \"SET enable_profiling = 'json';\n\" >> {sql_file_path}")
+    os.system(f"echo \"SET profiling_output = '{get_profile_json_path(q)}';\n\" >> {sql_file_path}")
+    os.system(f"cat {input_file} >> {sql_file_path}")
+    samples[f"{q}"] = []
+    
+    for _ in range(SAMPLES):
+        os.system(f"cat {sql_file_path} | {duckdb_exe} {tpch_db}")
+        profile = get_profile_json(q)
+        latency = profile["latency"]
+        ignition_cputime = find_ignition_entry(profile)["operator_timing"]
+        samples[f"{q}"].append([latency, ignition_cputime])
+
+
+with open(f"{out_dir}/tpch-t{threads}-results.json", 'w') as file:
+    json.dump(samples, file)
---
+++ scripts/tpch-create.sql
@@ -0,0 +1,101 @@
+create table part (
+    p_partkey     integer        not null,
+    p_name        varchar(55)    not null,
+    p_mfgr        char(25)       not null,
+    p_brand       char(10)       not null,
+    p_type        varchar(25)    not null,
+    p_size        integer        not null,
+    p_container   char(10)       not null,
+    p_retailprice decimal(12, 2) not null,
+    p_comment     varchar(23)    not null,
+    primary key (p_partkey)
+);
+
+create table region (
+    r_regionkey integer      not null,
+    r_name      char(25)     not null,
+    r_comment   varchar(152) not null,
+    primary key (r_regionkey)
+);
+
+create table nation (
+    n_nationkey integer      not null,
+    n_name      char(25)     not null,
+    n_regionkey integer      not null,
+    n_comment   varchar(152) not null,
+    primary key (n_nationkey)
+);
+
+create table supplier (
+    s_suppkey   integer        not null,
+    s_name      char(25)       not null,
+    s_address   varchar(40)    not null,
+    s_nationkey integer        not null,
+    s_phone     char(15)       not null,
+    s_acctbal   decimal(12, 2) not null,
+    s_comment   varchar(101)   not null,
+    primary key (s_suppkey)
+);
+
+create table partsupp (
+    ps_partkey    integer        not null,
+    ps_suppkey    integer        not null,
+    ps_availqty   integer        not null,
+    ps_supplycost decimal(12, 2) not null,
+    ps_comment    varchar(199)   not null,
+    primary key (ps_partkey, ps_suppkey)
+);
+
+create table customer (
+    c_custkey    integer        not null,
+    c_name       varchar(25)    not null,
+    c_address    varchar(40)    not null,
+    c_nationkey  integer        not null,
+    c_phone      char(15)       not null,
+    c_acctbal    decimal(12, 2) not null,
+    c_mktsegment char(10)       not null,
+    c_comment    varchar(117)   not null,
+    primary key (c_custkey)
+);
+
+create table orders (
+    o_orderkey      integer        not null,
+    o_custkey       integer        not null,
+    o_orderstatus   char(1)        not null,
+    o_totalprice    decimal(12, 2) not null,
+    o_orderdate     date           not null,
+    o_orderpriority char(15)       not null,
+    o_clerk         char(15)       not null,
+    o_shippriority  integer        not null,
+    o_comment       varchar(79)    not null,
+    primary key (o_orderkey)
+);
+
+COPY part FROM '../tpch-dbgen/scale1/part.tbl' WITH (DELIMITER '|');
+COPY region FROM '../tpch-dbgen/scale1/region.tbl' WITH (DELIMITER '|');
+COPY nation FROM '../tpch-dbgen/scale1/nation.tbl' WITH (DELIMITER '|');
+COPY supplier FROM '../tpch-dbgen/scale1/supplier.tbl' WITH (DELIMITER '|');
+COPY partsupp FROM '../tpch-dbgen/scale1/partsupp.tbl' WITH (DELIMITER '|');
+COPY customer FROM '../tpch-dbgen/scale1/customer.tbl' WITH (DELIMITER '|');
+COPY orders FROM '../tpch-dbgen/scale1/orders.tbl' WITH (DELIMITER '|');
+
+CREATE VIEW lineitem AS (
+ SELECT
+ l_orderkey,
+ l_partkey,
+ l_suppkey,
+ l_linenumber,
+ CAST(l_quantity AS DECIMAL(12, 2)) / 100.0 AS l_quantity,
+ CAST(l_extendedprice AS DECIMAL(12, 2)) / 100.0 AS l_extendedprice,
+ CAST(l_discount AS DECIMAL(12, 2)) / 100.0 AS l_discount,
+ CAST(l_tax AS DECIMAL(12, 2)) / 100.0 AS l_tax,
+ CHR(l_returnflag) AS l_returnflag,
+ CHR(l_linestatus) AS l_linestatus,
+ l_shipdate,
+ l_commitdate,
+ l_receiptdate,
+ l_shipinstruct,
+ l_shipmode,
+ l_comment
+ FROM ignition('/home/mat/src/portable-decompress/dataset/tpch-vortex.ignition')
+);
