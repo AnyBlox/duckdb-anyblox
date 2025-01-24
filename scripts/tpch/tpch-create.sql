918edc5efe193c9eead09bd87e53fa637c3cc05b
Mateusz Gienieczko <giem@in.tum.de>
2025-01-24 02:10:31 +0100

    update script to the workstation version

--- scripts/tpch/tpch-create.sql
+++ scripts/tpch/tpch-create.sql
@@ -6,7 +6,7 @@ create table part (
     p_type        varchar(25)    not null,
     p_size        integer        not null,
     p_container   char(10)       not null,
-    p_retailprice decimal(12, 2) not null,
+    p_retailprice double         not null,
     p_comment     varchar(23)    not null,
     primary key (p_partkey)
 );
@@ -32,7 +32,7 @@ create table supplier (
     s_address   varchar(40)    not null,
     s_nationkey integer        not null,
     s_phone     char(15)       not null,
-    s_acctbal   decimal(12, 2) not null,
+    s_acctbal   double         not null,
     s_comment   varchar(101)   not null,
     primary key (s_suppkey)
 );
@@ -41,7 +41,7 @@ create table partsupp (
     ps_partkey    integer        not null,
     ps_suppkey    integer        not null,
     ps_availqty   integer        not null,
-    ps_supplycost decimal(12, 2) not null,
+    ps_supplycost double         not null,
     ps_comment    varchar(199)   not null,
     primary key (ps_partkey, ps_suppkey)
 );
@@ -52,7 +52,7 @@ create table customer (
     c_address    varchar(40)    not null,
     c_nationkey  integer        not null,
     c_phone      char(15)       not null,
-    c_acctbal    decimal(12, 2) not null,
+    c_acctbal    double         not null,
     c_mktsegment char(10)       not null,
     c_comment    varchar(117)   not null,
     primary key (c_custkey)
@@ -62,7 +62,7 @@ create table orders (
     o_orderkey      integer        not null,
     o_custkey       integer        not null,
     o_orderstatus   char(1)        not null,
-    o_totalprice    decimal(12, 2) not null,
+    o_totalprice    double         not null,
     o_orderdate     date           not null,
     o_orderpriority char(15)       not null,
     o_clerk         char(15)       not null,
@@ -85,12 +85,12 @@ CREATE VIEW lineitem AS (
  l_partkey,
  l_suppkey,
  l_linenumber,
- CAST(l_quantity AS DECIMAL(12, 2)) / 100.0 AS l_quantity,
- CAST(l_extendedprice AS DECIMAL(12, 2)) / 100.0 AS l_extendedprice,
- CAST(l_discount AS DECIMAL(12, 2)) / 100.0 AS l_discount,
- CAST(l_tax AS DECIMAL(12, 2)) / 100.0 AS l_tax,
- CHR(l_returnflag) AS l_returnflag,
- CHR(l_linestatus) AS l_linestatus,
+ l_quantity,
+ l_extendedprice,
+ l_discount,
+ l_tax,
+ l_returnflag,
+ l_linestatus,
  l_shipdate,
  l_commitdate,
  l_receiptdate,
